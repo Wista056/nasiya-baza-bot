f903c5f516617243782e5c3e80bffd910d26c32a
Sarvarbek <sarvarbek.subxonov2202@gmail.com>
2026-06-02 00:14:59 +0500

    Fix config

--- config.py
+++ config.py
@@ -3,5 +3,4 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
 ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "6302553503").split(",")]
-
-CHANNEL_ID = -1003903723271
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003903723271"))
