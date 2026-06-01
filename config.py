f4e7420a7ec75991d51b604852352e7cd4cfa4ff
Sarvarbek <sarvarbek.subxonov2202@gmail.com>
2026-06-02 00:07:49 +0500

    Add channel ID

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
 ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "6302553503").split(",")]
+
+CHANNEL_ID = -1003903723271
