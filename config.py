dd9975e8b6c898b8e3bd938c3419ef298ab3d6cb
Sarvarbek <sarvarbek.subxonov2202@gmail.com>
2026-06-02 08:46:27 +0500

    Add second admin

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
-ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "6302553503").split(",")]
+ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "6302553503,8535947057").split(",")]
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003903723271"))
